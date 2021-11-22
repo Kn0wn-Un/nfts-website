4f32c73410304269a9b3cd3e4b62bc77dde5e7ce
Darshan H M <naikdarsh175@gmail.com>
2021-11-22 21:06:03 +0530

    events and user mapping added

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -12,7 +12,10 @@ import {Base64} from "./libraries/Base64.sol";
 contract MyEpicNFT is ERC721URIStorage {
     // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
     using Counters for Counters.Counter;
+    event NewEpicNFTMinted(address sender, uint256 tokenId);
+
     Counters.Counter private _tokenIds;
+    mapping(address => uint256) public userToNft;
 
     string baseSvg =
         "<svg width='300' height='250' xmlns='http://www.w3.org/2000/svg' version='1.0' style='background-color:#000'><defs><linearGradient id='c' x1='0' y1='0' x2='1' y2='1'><stop stop-color='#f0f' stop-opacity='.996' offset='0'/><stop stop-color='#00f' stop-opacity='.996' offset='1'/></linearGradient><linearGradient id='a' x1='0' y1='0' x2='1' y2='1'><stop stop-color='#f0f' offset='0'/><stop stop-color='#00f' stop-opacity='.996' offset='1'/><stop offset='1' stop-opacity='.988'/><stop stop-color='0' stop-opacity='0' offset='NaN'/><stop stop-color='0' stop-opacity='0' offset='NaN'/></linearGradient><filter id='b' x='-50%' y='-50%' width='200%' height='200%'><feGaussianBlur in='SourceGraphic' stdDeviation='10'/></filter></defs><path fill='url(#a)' filter='url(#b)' d='M150 244c-69.06 0-125-55.94-125-125S80.94-6 150-6s125 55.94 125 125-55.94 125-125 125z' opacity='.35'/><path fill='url(#c)' opacity='undefined' d='M150 214c-49.724 0-90-40.276-90-90s40.276-90 90-90 90 40.276 90 90-40.276 90-90 90z'/><path fill='none' stroke='#000' d='M0 150h300M0 160h300M0 170h300M0 180h300M0 190h300M0 200h300M0 210h300'/><text fill='#fff' stroke='#000' stroke-width='0' x='235' y='250' font-size='18' font-family='Caveat' xml:space='preserve'>";
@@ -20,6 +23,7 @@ contract MyEpicNFT is ERC721URIStorage {
     // We need to pass the name of our NFTs token and it's symbol.
     constructor() ERC721("NeonSunNFT", "NEONSUN") {
         console.log("This is my NFT contract. Woah!");
+        _tokenIds.increment();
     }
 
     // A function our user will hit to get their NFT.
@@ -27,14 +31,17 @@ contract MyEpicNFT is ERC721URIStorage {
         // Get the current tokenId, this starts at 0.
         uint256 newItemId = _tokenIds.current();
 
+        require(newItemId <= 50, "NFT Limit Reached");
+        require(userToNft[msg.sender] == 0, "User already owns NFT");
+
         // Actually mint the NFT to the sender using msg.sender.
         _safeMint(msg.sender, newItemId);
 
         string memory finalSvg = string(
             abi.encodePacked(
                 baseSvg,
-                Strings.toString(_tokenIds.current() + 1),
-                "/100",
+                Strings.toString(_tokenIds.current()),
+                "/50",
                 "</text></svg>"
             )
         );
@@ -45,7 +52,7 @@ contract MyEpicNFT is ERC721URIStorage {
                     abi.encodePacked(
                         '{"name": "Neon-Sun #',
                         // We set the title of our NFT as the generated word.
-                        Strings.toString(_tokenIds.current() + 1),
+                        Strings.toString(_tokenIds.current()),
                         '", "description": "Collection of neon suns.", "image": "data:image/svg+xml;base64,',
                         // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
                         Base64.encode(bytes(finalSvg)),
@@ -67,7 +74,19 @@ contract MyEpicNFT is ERC721URIStorage {
             msg.sender
         );
 
+        userToNft[msg.sender] = newItemId;
+
         // Increment the counter for when the next NFT is minted.
         _tokenIds.increment();
+
+        emit NewEpicNFTMinted(msg.sender, newItemId);
+    }
+
+    function totalSupply() public view returns (uint256 _totalnfts) {
+        return _tokenIds.current() - 1;
+    }
+
+    function getUserNft() public view returns (uint256 _nftId) {
+        return userToNft[msg.sender];
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -4,15 +4,19 @@ const main = async () => {
 	await nftContract.deployed();
 	console.log('Contract deployed to:', nftContract.address);
 
+	let userNFT = await nftContract.getUserNft();
+	console.log(userNFT.toNumber());
+
 	// Call the function.
-	// let txn = await nftContract.makeAnEpicNFT();
-	// // Wait for it to be mined.
-	// await txn.wait();
-	//
-	// // Mint another NFT for fun.
-	// txn = await nftContract.makeAnEpicNFT();
-	// // Wait for it to be mined.
-	// await txn.wait();
+	let txn = await nftContract.makeAnEpicNFT();
+	// Wait for it to be mined.
+	await txn.wait();
+
+	const totalSupply = await nftContract.totalSupply();
+	console.log(totalSupply.toString());
+
+	userNFT = await nftContract.getUserNft();
+	console.log(userNFT.toNumber());
 };
 
 const runMain = async () => {
